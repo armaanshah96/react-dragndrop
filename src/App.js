5a62986592e024b3d9a0c0e4dfb796856b295a10
Armaan Shah <armaanshah96@gmail.com>
2018-03-11 20:56:15 -0500

    basic flexbox

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <List />
+        <div id="list-outer">
+          <List />
+        </div>
       </div>
     );
   }
